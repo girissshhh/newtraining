953acf4c6d72d3e7c32bc796750e0d843d43e3da
girissshh <girissshh22@gmail.com>
2025-12-18 09:40:50 +0530

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,29 @@
+#DATA TYPES
+
+#INTEGER
+a = 13
+b = 2
+print("Integer Division:", a // b)  # O/p: 6
+print("Float Division:", a / b)      # o/p: 6.5
+print("Modulus:", a % b)             # Outputs: 1
+print("Exponentiation:", a ** b)     # Outputs: 169
+print("Type of a:", type(a))          # Outputs: <class 'int'>
+print("Type of b:", type(b))          # Outputs: <class 'int'>
+print("Boolean of a:", bool(a))        # Outputs: True
+print("Boolean of 0:", bool(0))        # Outputs: False
+print("Binary of a:", bin(a))        # Outputs: 0b1101
+print("Hexadecimal of a:", hex(a))   # Outputs: 0xd
+print("Octal of a:", oct(a))         # Outputs: 0o15
+
+
+#variables 
+x = 10
+y = 5
+sum = x + y
+
+print("Sum:", sum)  # Outputs: 15
+diff = x - y
+print("Difference:", diff)  # Outputs: 5
+
+
+
